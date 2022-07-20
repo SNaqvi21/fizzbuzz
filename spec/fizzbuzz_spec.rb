ff89bd5990e93ba66a6bc286daac181dca2b093b
Syed Naqvi <snaqvi21@hotmail.com>
2022-07-20 11:31:47 +0100

    fizzbuzz.rb

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,19 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "Fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'Fizz'
+  end
+
+  it 'returns 1 when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+
+  it 'returns Buzz when passed 5' do
+    expect(fizzbuzz(5)).to eq "Buzz"
+  end
+
+  it 'returns FizzBuzz when passed 15' do
+    expect(fizzbuzz(15)).to eq "FizzBuzz"
   end
 end
